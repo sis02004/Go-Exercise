68486a6a6b9aeb5c9769e51a55fd2f487cee5f4a
sis02004 <sis02004@naver.com>
2018-07-26 13:51:17 +0900

    channel exercise

---
+++ channel-3.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+  "fmt"
+  "runtime"
+)
+
+func main(){
+  runtime.GOMAXPROCS(1)
+
+  done := make(chan bool, 2)
+  count := 4
+
+  go func(){
+    for i := 0; i < count; i++{
+      done <- true
+      fmt.Println("고루틴 : ", i)
+    }
+  }()
+
+  for i := 0; i < count; i++{
+    <-done
+    fmt.Println("메인 함수 : ", i)
+  }
+}
